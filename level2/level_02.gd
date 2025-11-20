73cb13a489cc684a4b5f0cc5d52584fe53e98b0b
Yahir <leonardocayola@gmail.com>
2025-11-19 21:23:01 -0400

    Revert "Se añadio modal de reinicio al llegar las monedas a 0 y se cambio el diseño de las monedas"
    This reverts commit a7e84b11912825d5fb38def0bbc95da04dbd1ec2.

--- level2/level_02.gd
+++ level2/level_02.gd
@@ -10,7 +10,6 @@ extends Node2D
 var hub_objective_scene: PackedScene = preload("res://ui/barra_objetivos/hub_objetive.tscn")
 var hub_objective: Node2D
 
-const RestartScene = preload("res://level_modal/reinicioNivel.tscn")
 var delete_mode: bool = false
 
 func _ready() -> void:
@@ -364,15 +363,4 @@ func _on_game_over_no_money() -> void:
 	
 	print("💀 ¡PERDISTE EL JUEGO! 💀")
 	
-	# 1. Pausa el juego
-	get_tree().paused = true
-	
-	# 2. Crea (instancia) la escena de reinicio
-	var restart_instance = RestartScene.instantiate()
-	
-	# 3. IMPORTANTE: Hace que la ventana de reinicio funcione aunque el juego esté pausado
-	restart_instance.process_mode = Node.PROCESS_MODE_ALWAYS
-	
-	# 4. Añade la ventana a la escena (usamos 'root' para asegurarnos que esté encima de todo)
-	get_tree().root.add_child(restart_instance)
 	
